e5f43aa46f6434e536e1d9f613ed9344f88b2e7b
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2016-09-02 12:24:08 -0700

    Move storage logic to entity storage client.
    BACKEND-496

--- src/main/java/com/kryptnostic/datastore/odata/KryptnosticEntityCollectionProcessor.java
+++ src/main/java/com/kryptnostic/datastore/odata/KryptnosticEntityCollectionProcessor.java
@@ -31,10 +31,16 @@ import org.apache.olingo.server.api.uri.UriResource;
 import org.apache.olingo.server.api.uri.UriResourceEntitySet;
 
 import com.kryptnostic.datastore.Datastore;
+import com.kryptnostic.datastore.services.EntityStorageClient;
 
 public class KryptnosticEntityCollectionProcessor implements EntityCollectionProcessor {
-    private OData           odata;
-    private ServiceMetadata serviceMetadata;
+    private OData                     odata;
+    private ServiceMetadata           serviceMetadata;
+    private final EntityStorageClient storage;
+
+    public KryptnosticEntityCollectionProcessor( EntityStorageClient storage ) {
+        this.storage = storage;
+    }
 
     @Override
     public void init( OData odata, ServiceMetadata serviceMetadata ) {
@@ -54,12 +60,12 @@ public class KryptnosticEntityCollectionProcessor implements EntityCollectionPro
         UriResourceEntitySet uriResourceEntitySet = (UriResourceEntitySet) resourcePaths.get( 0 ); // in our example,
                                                                                                    // the first segment
                                                                                                    // is the EntitySet
-        
+
         EdmEntitySet edmEntitySet = uriResourceEntitySet.getEntitySet();
 
         // 2nd: fetch the data from backend for this requested EntitySetName
         // it has to be delivered as EntitySet object
-        EntityCollection entitySet = getData( edmEntitySet );
+        EntityCollection entitySet = storage.readEntitySetData( edmEntitySet );
 
         // 3rd: create a serializer based on the requested format (json)
         ODataSerializer serializer = odata.createSerializer( responseFormat );
@@ -71,7 +77,7 @@ public class KryptnosticEntityCollectionProcessor implements EntityCollectionPro
         final String id = request.getRawBaseUri() + "/" + edmEntitySet.getName();
         EntityCollectionSerializerOptions opts = EntityCollectionSerializerOptions.with().id( id )
                 .contextURL( contextUrl ).build();
-        
+
         SerializerResult serializerResult = serializer.entityCollection( serviceMetadata,
                 edmEntityType,
                 entitySet,
@@ -84,56 +90,5 @@ public class KryptnosticEntityCollectionProcessor implements EntityCollectionPro
         response.setHeader( HttpHeader.CONTENT_TYPE, responseFormat.toContentTypeString() );
     }
 
-    private EntityCollection getData( EdmEntitySet edmEntitySet ) {
-        EntityCollection productsCollection = new EntityCollection();
-        // check for which EdmEntitySet the data is requested
-        if ( Datastore.ES_PRODUCTS_NAME.equals( edmEntitySet.getName() ) ) {
-            List<Entity> productList = productsCollection.getEntities();
-
-            // add some sample product entities
-            final Entity e1 = new Entity()
-                    .addProperty( new Property( null, "ID", ValueType.PRIMITIVE, 1 ) )
-                    .addProperty( new Property( null, "Name", ValueType.PRIMITIVE, "Notebook Basic 15" ) )
-                    .addProperty( new Property(
-                            null,
-                            "Description",
-                            ValueType.PRIMITIVE,
-                            "Notebook Basic, 1.7GHz - 15 XGA - 1024MB DDR2 SDRAM - 40GB" ) );
-            e1.setId( createId( "Products", 1 ) );
-            productList.add( e1 );
-
-            final Entity e2 = new Entity()
-                    .addProperty( new Property( null, "ID", ValueType.PRIMITIVE, 2 ) )
-                    .addProperty( new Property( null, "Name", ValueType.PRIMITIVE, "1UMTS PDA" ) )
-                    .addProperty( new Property(
-                            null,
-                            "Description",
-                            ValueType.PRIMITIVE,
-                            "Ultrafast 3G UMTS/HSDPA Pocket PC, supports GSM network" ) );
-            e2.setId( createId( "Products", 1 ) );
-            productList.add( e2 );
-
-            final Entity e3 = new Entity()
-                    .addProperty( new Property( null, "ID", ValueType.PRIMITIVE, 3 ) )
-                    .addProperty( new Property( null, "Name", ValueType.PRIMITIVE, "Ergo Screen" ) )
-                    .addProperty( new Property(
-                            null,
-                            "Description",
-                            ValueType.PRIMITIVE,
-                            "19 Optimum Resolution 1024 x 768 @ 85Hz, resolution 1280 x 960" ) );
-            e3.setId( createId( "Products", 1 ) );
-            productList.add( e3 );
-        }
-
-        return productsCollection;
-    }
-
-    private URI createId( String entitySetName, Object id ) {
-        try {
-            return new URI( entitySetName + "(" + String.valueOf( id ) + ")" );
-        } catch ( URISyntaxException e ) {
-            throw new ODataRuntimeException( "Unable to create id for entity: " + entitySetName, e );
-        }
-    }
 
 }

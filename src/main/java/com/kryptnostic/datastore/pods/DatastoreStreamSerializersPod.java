70de3027583ebcf19401d81c59fee78f0b977068
Katherine <katherine@kryptnostic.com>
2016-09-19 14:00:34 -0700

    inject api in DatastoreStreamSerializersPod

--- src/main/java/com/kryptnostic/datastore/pods/DatastoreStreamSerializersPod.java
+++ src/main/java/com/kryptnostic/datastore/pods/DatastoreStreamSerializersPod.java
@@ -1,18 +1,24 @@
 package com.kryptnostic.datastore.pods;
 
+import javax.inject.Inject;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.kryptnostic.conductor.rpc.ConductorSparkApi;
 import com.kryptnostic.conductor.rpc.LambdaStreamSerializer;
 import com.kryptnostic.conductor.rpc.serializers.ConductorCallStreamSerializer;
 import com.kryptnostic.conductor.rpc.serializers.EmployeeStreamSerializer;
 
 @Configuration
 public class DatastoreStreamSerializersPod {
+	
+	@Inject
+	private ConductorSparkApi api;
 
     @Bean
     public ConductorCallStreamSerializer ccss() {
-        return new ConductorCallStreamSerializer( null );
+        return new ConductorCallStreamSerializer( api );
     }
 
     @Bean

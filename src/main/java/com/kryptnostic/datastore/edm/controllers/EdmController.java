11488d307f393fc1c8463461defcb8f23f949a52
Yao <yao@kryptnostic.com>
2016-09-28 14:51:33 -0700

    internal changes about delete entityType

--- src/main/java/com/kryptnostic/datastore/edm/controllers/EdmController.java
+++ src/main/java/com/kryptnostic/datastore/edm/controllers/EdmController.java
@@ -244,7 +244,7 @@ public class EdmController implements EdmApi {
             method = RequestMethod.DELETE )
     @ResponseStatus( HttpStatus.OK )
     public Response deleteEntityType( @PathVariable( NAMESPACE ) String namespace, @PathVariable( NAME ) String name ) {
-        modelService.deleteEntityType( new EntityType().setNamespace( namespace ).setName( name ) );
+        modelService.deleteEntityType( new FullQualifiedName( namespace, name ) );
         return null;
     }
 

aacde355ba3167229338a5341631916c4aa62bed
Ho Chung Siu <hochung@kryptnostic.com>
2016-09-30 15:23:02 -0700

    Updated population script, not depending on external site anymore

--- src/test/java/com/kryptnostic/datastore/edm/CustomCSVPopulation.java
+++ src/test/java/com/kryptnostic/datastore/edm/CustomCSVPopulation.java
@@ -1,9 +1,13 @@
 package com.kryptnostic.datastore.edm;
 
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -15,6 +19,9 @@ import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.UUID;
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.olingo.commons.api.data.Entity;
@@ -26,10 +33,12 @@ import org.apache.olingo.commons.api.edm.FullQualifiedName;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectWriter;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvParser;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema;
+import com.fasterxml.jackson.dataformat.csv.CsvSchema.ColumnType;
 import com.google.common.collect.ImmutableSet;
-import com.kryptnostic.conductor.rpc.Employee;
 import com.kryptnostic.conductor.rpc.UUIDs.ACLs;
 import com.kryptnostic.conductor.rpc.UUIDs.Syncs;
 import com.kryptnostic.conductor.rpc.odata.EntityType;
@@ -48,9 +57,16 @@ public class CustomCSVPopulation {
 	public static List<Integer> multiplicityOfDefaultType = new ArrayList<Integer>();
 	
 	public static List<customPropertyType> propertyTypesList = new ArrayList<customPropertyType>();
+	public static CsvSchema csvSchema;
 	public static List<String> EntityTypesList = new ArrayList<String>();
 	public static List<String> EntitySetsList = new ArrayList<String>();
-	public static Map<String, String> EntitySetToType = new HashMap<String, String>();
+	public static Map<String, String> EntitySetToType = new HashMap<String, String>();	
+	
+	//Random
+	public static Map<String, Supplier> RandomGenerator = new HashMap<String, Supplier>();
+	
+	//Partition Key
+	public static int partitionKey = 0;
 	
 	/**
 	 * Custom PropertyType. Will use to generate PropertyType in datastore.
@@ -63,98 +79,73 @@ public class CustomCSVPopulation {
 	private static class customPropertyType{
 		private String name;
 		private EdmPrimitiveTypeKind dataType;
-		private String typeInURL;
+		private String keyword;
+//		private ColumnType columnType;
+		private Callable randomGenCallable;
+		private String javaTypeName;
 		
-		public customPropertyType(String name, EdmPrimitiveTypeKind dataType, String typeInURL) {
+		public customPropertyType(String name, EdmPrimitiveTypeKind dataType, String keyword, Callable randomGenCallable, String javaTypeName) {
 			this.name = name;
 			this.dataType = dataType;
-			this.typeInURL = typeInURL;
+			this.keyword = keyword;
+//			this.columnType = columnType;
+			this.randomGenCallable = randomGenCallable;
+			this.javaTypeName = javaTypeName;
 		}
 		
+		public String getJavaTypeName() {
+			return javaTypeName;
+		}
+
+//		public ColumnType getColumnType() {
+//			return columnType;
+//		}
+
+		public String getKeyword() {
+			return keyword;
+		}
+
 		public String getName() {
 			return name;
 		}
-		public void setName(String name) {
-			this.name = name;
-		}
+
 		public EdmPrimitiveTypeKind getDataType() {
 			return dataType;
 		}
-		public void setDataType(EdmPrimitiveTypeKind dataType) {
-			this.dataType = dataType;
+		
+		public Callable getRandomGenCallable(){
+			return randomGenCallable;
 		}
-		public String getTypeInURL() {
-			return typeInURL;
-		}
-		public void setTypeInURL(String typeInURL) {
-			this.typeInURL = typeInURL;
+		public Object getRandom() throws Exception{
+			return randomGenCallable.call();
 		}
 	}
 	
 	public static void LoadDefaultPropertyTypes(){
-		defaultTypeList.add( new customPropertyType("age", EdmPrimitiveTypeKind.Int16, "age") );
+		defaultTypeList.add( new customPropertyType("age", EdmPrimitiveTypeKind.Int32, "age", () -> (new Random()).nextInt(120), "Integer") );
 		multiplicityOfDefaultType.add(0);
 		
-		defaultTypeList.add( new customPropertyType("alpha", EdmPrimitiveTypeKind.String, "alpha") );
+		defaultTypeList.add( new customPropertyType("alpha", EdmPrimitiveTypeKind.String, "alpha", () -> RandomStringUtils.randomAlphabetic(8), "String" ) );
+		multiplicityOfDefaultType.add(0);
+				
+		defaultTypeList.add( new customPropertyType("bool", EdmPrimitiveTypeKind.Boolean, "bool", () -> (new Random()).nextBoolean(), "Boolean" ) );
 		multiplicityOfDefaultType.add(0);
 		
-		defaultTypeList.add( new customPropertyType("birthday", EdmPrimitiveTypeKind.Date, "birthday") );
+		defaultTypeList.add( new customPropertyType("char", EdmPrimitiveTypeKind.String, "char", () -> RandomStringUtils.randomAlphabetic(1), "String" ) );
 		multiplicityOfDefaultType.add(0);
 		
-		defaultTypeList.add( new customPropertyType("bool", EdmPrimitiveTypeKind.Boolean, "bool") );
+		defaultTypeList.add( new customPropertyType("float", EdmPrimitiveTypeKind.Double, "float", () -> (new Random()).nextFloat(), "Double" ) );
 		multiplicityOfDefaultType.add(0);
 		
-		defaultTypeList.add( new customPropertyType("char", EdmPrimitiveTypeKind.String, "char") );
+		defaultTypeList.add( new customPropertyType("guid", EdmPrimitiveTypeKind.Guid, "guid", () -> UUID.randomUUID(), "UUID" ) );
 		multiplicityOfDefaultType.add(0);
 		
-		defaultTypeList.add( new customPropertyType("city", EdmPrimitiveTypeKind.String, "city") );
+		defaultTypeList.add( new customPropertyType("integer", EdmPrimitiveTypeKind.Int32, "integer", () -> (new Random()).nextInt(12345678), "Integer" ) );
 		multiplicityOfDefaultType.add(0);
 		
-		defaultTypeList.add( new customPropertyType("ccnumber", EdmPrimitiveTypeKind.Int64, "ccnumber") );
+		defaultTypeList.add( new customPropertyType("string", EdmPrimitiveTypeKind.String, "string", () -> RandomStringUtils.randomAscii(10), "String" ) );
 		multiplicityOfDefaultType.add(0);
-		
-		defaultTypeList.add( new customPropertyType("date", EdmPrimitiveTypeKind.Date, "date") );
-		multiplicityOfDefaultType.add(0);
-		
-		defaultTypeList.add( new customPropertyType("digit", EdmPrimitiveTypeKind.Byte, "digit") );
-		multiplicityOfDefaultType.add(0);
-		
-		defaultTypeList.add( new customPropertyType("dollar", EdmPrimitiveTypeKind.String, "dollar") );
-		multiplicityOfDefaultType.add(0);
-		
-		defaultTypeList.add( new customPropertyType("email", EdmPrimitiveTypeKind.String, "email") );
-		multiplicityOfDefaultType.add(0);
-		
-		defaultTypeList.add( new customPropertyType("first", EdmPrimitiveTypeKind.String, "first") );
-		multiplicityOfDefaultType.add(0);
-		
-		defaultTypeList.add( new customPropertyType("float", EdmPrimitiveTypeKind.Double, "float") );
-		multiplicityOfDefaultType.add(0);
-		
-		defaultTypeList.add( new customPropertyType("gender", EdmPrimitiveTypeKind.String, "gender") );
-		multiplicityOfDefaultType.add(0);
-		
-		defaultTypeList.add( new customPropertyType("guid", EdmPrimitiveTypeKind.Guid, "guid") );
-		multiplicityOfDefaultType.add(0);
-		
-		defaultTypeList.add( new customPropertyType("integer", EdmPrimitiveTypeKind.Int64, "integer") );
-		multiplicityOfDefaultType.add(0);
-		
-		defaultTypeList.add( new customPropertyType("last", EdmPrimitiveTypeKind.String, "last") );
-		multiplicityOfDefaultType.add(0);
-		
-		defaultTypeList.add( new customPropertyType("name", EdmPrimitiveTypeKind.String, "name") );
-		multiplicityOfDefaultType.add(0);
-		
-		defaultTypeList.add( new customPropertyType("string", EdmPrimitiveTypeKind.String, "string") );
-		multiplicityOfDefaultType.add(0);
-
-		defaultTypeList.add( new customPropertyType("street", EdmPrimitiveTypeKind.String, "street") );
-		multiplicityOfDefaultType.add(0);
-		
-		defaultTypeList.add( new customPropertyType("state", EdmPrimitiveTypeKind.String, "state") );
-		multiplicityOfDefaultType.add(0);
-		
+				
 		defaultTypeSize = defaultTypeList.size();				
 	}
 	
@@ -174,22 +165,52 @@ public class CustomCSVPopulation {
 			int count = multiplicityOfDefaultType.get( index ) + 1;
 			multiplicityOfDefaultType.set( index, count );
 			
-			String newName = defaultTypeList.get(index).getName() + "-" + count;
-			EdmPrimitiveTypeKind dataType = defaultTypeList.get(index).getDataType();
-			String typeInURL = defaultTypeList.get(index).getTypeInURL();
+			customPropertyType propertyType = defaultTypeList.get(index);
+			String newName = propertyType.getName() + "-" + count;
+			EdmPrimitiveTypeKind dataType = propertyType.getDataType();
+			String keyword = propertyType.getKeyword();
+//			ColumnType columnType = propertyType.getColumnType();
+			Callable randomGenCallable = propertyType.getRandomGenCallable();
+			String javaTypeName = propertyType.getJavaTypeName();
 			
-			propertyTypesList.add( new customPropertyType(newName,dataType,typeInURL) );			
+			propertyTypesList.add( new customPropertyType(newName,dataType,keyword,randomGenCallable,javaTypeName) );			
 		}
 		return propertyTypesList;
 	}
 	
-	public static void GenerateHeader(){
-		String JSONvalue = "";
-		for(customPropertyType type : propertyTypesList){
-			JSONvalue = JSONvalue + type.getTypeInURL() + ",";
+	public static void GenerateCSV(int numRows, String location) throws Exception{
+		//Build CSV Schema
+		CsvSchema.Builder schemaBuilder = CsvSchema.builder();
+		for (customPropertyType type: propertyTypesList){
+//			schemaBuilder.addColumn(type.getName(), type.getColumnType());
+			schemaBuilder.addColumn(type.getName());
 		}
-		JSONvalue = JSONvalue.substring(0, JSONvalue.length() - 1);
-		System.out.println(JSONvalue);
+		csvSchema = schemaBuilder.build();
+		
+		//Write to CSV
+        CsvMapper mapper = new CsvMapper();
+        ObjectWriter myObjectWriter = mapper.writer(csvSchema);
+        
+        File tempFile = new File(location);
+        if( tempFile.exists() ){
+        	tempFile.delete();
+        }
+        tempFile.createNewFile();
+        
+        FileOutputStream tempFileOutputStream = new FileOutputStream(tempFile);
+        BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(tempFileOutputStream, 1024);
+        OutputStreamWriter writerOutputStream = new OutputStreamWriter(bufferedOutputStream, "UTF-8");
+        
+        List<List<Object>> values = new ArrayList<List<Object>>();
+        for(int i = 0; i < numRows; i++){
+        	List<Object> rowValues = new ArrayList<Object>();
+        	for( customPropertyType type : propertyTypesList){
+        		rowValues.add( type.getRandom() );
+        	}
+        	values.add(rowValues);
+        }
+        myObjectWriter.writeValue(writerOutputStream, values);
+		System.out.println("CSV generated \n");
 	}
 	
 	public static void CreatePropertyTypes(){
@@ -214,7 +235,7 @@ public class CustomCSVPopulation {
 			
 	        EntityType entityType = new EntityType().setNamespace( NAMESPACE )
 	        		.setName( entityTypeName )
-	                .setKey( ImmutableSet.of( new FullQualifiedName( NAMESPACE, "seq" ) ) );
+	                .setKey( ImmutableSet.of( new FullQualifiedName( NAMESPACE, "key" ) ) );
 	        //Add property types to entity type
 	        
 	        Set<FullQualifiedName> setPropertyTypesFQN = new HashSet<FullQualifiedName>();
@@ -259,6 +280,23 @@ public class CustomCSVPopulation {
                 setOfEntityTypes );
 	}
 	
+	private static Object TypeConversion(String str, String type){
+		//Convert string to the corresponding type, guaranteed that the string can be converted to that cype.
+		if(type.equals("Integer")){
+			return Integer.parseInt(str);
+		}else if(type.equals("Boolean")){
+			return Boolean.parseBoolean(str);			
+		}else if(type.equals("Byte")){
+			return Byte.parseByte(str);
+		}else if(type.equals("Double")){
+			return Double.parseDouble(str);
+		}else if(type.equals("UUID")){
+			return UUID.fromString(str);
+		}else{
+			return str;
+		}		
+	}
+	
 	public static void WriteCSVtoDB(String location) throws JsonProcessingException, IOException{
 		int numOfEntitySets = EntitySetsList.size();
 		Random rand = new Random();
@@ -268,28 +306,26 @@ public class CustomCSVPopulation {
 		mapper.enable(CsvParser.Feature.WRAP_AS_ARRAY);
 		File csvFile = new File( location ); // or from String, URL etc
 		
-		MappingIterator<List<Object>> it = mapper.readerFor(new TypeReference<List<Object>>() { }).readValues(csvFile);
+		MappingIterator<Map<String,String>> it = mapper.readerFor(new TypeReference<Map<String,String>>() { }).with(csvSchema).readValues(csvFile);
 		while (it.hasNext()) {
 		  Entity entity = new Entity();
 		  String entitySetName = EntitySetsList.get( rand.nextInt( numOfEntitySets ) );
 		  String entityTypeName = EntitySetToType.get( entitySetName );
 		  FullQualifiedName entityTypeFQN = new FullQualifiedName(NAMESPACE, entityTypeName);
 		  
-          entity.setType( entityTypeFQN.getFullQualifiedNameAsString() );			
-		  
-          Iterator<Object> rowIterator = it.next().iterator();
-		  
+          entity.setType( entityTypeFQN.getFullQualifiedNameAsString() );
+          Map<String, String> map = it.next();
+          
 		  for(customPropertyType propertyType : propertyTypesList){
               Property property = new Property();
               String propertyName = propertyType.getName();
               
               property.setName( propertyName );
               property.setType( new FullQualifiedName( NAMESPACE, propertyName ).getFullQualifiedNameAsString() );
-              property.setValue( ValueType.PRIMITIVE, rowIterator.next() );
+              property.setValue( ValueType.PRIMITIVE, TypeConversion(map.get(propertyName), propertyType.getJavaTypeName() ));
               
               entity.addProperty(property);
-		  }
-		  
+		  } 
           odsc.createEntityData( ACLs.EVERYONE_ACL,
                   Syncs.BASE.getSyncId(),
                   entitySetName,
@@ -297,23 +333,21 @@ public class CustomCSVPopulation {
                   entity );
 		}
 	}
-	public static void main(String args[]) throws JsonProcessingException, IOException{
+	public static void main(String args[]) throws Exception{
 		//Perhaps drop keyspace to make things cleaner
 		//DONE: Load the default property types, from part of the keywords in http://www.convertcsv.com/generate-test-data.htm#keywords
 		LoadDefaultPropertyTypes();
 		//DONE: Generate a list of custom property types
 		GeneratePropertyTypes(10);
-		//DONE: Add in a seq column that will be used as partition key
-		propertyTypesList.add( new customPropertyType("seq", EdmPrimitiveTypeKind.Int64, "seq") );
-		//DONE: Generate value for JSON Header - at this point, go to website http://www.convertcsv.com/generate-test-data.htm to generate csv
-		GenerateHeader();
-		
-		//Input Filename
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter Name of CSV file");
-        String nameCSV = scanner.nextLine();
-        
+		//DONE: Add in a key column that will be used as partition key
+		propertyTypesList.add( new customPropertyType("key", EdmPrimitiveTypeKind.Int64, "key", () -> ++partitionKey, "Integer" ) );
+		//Generate CSV
+		try {
+			GenerateCSV(100, "src/test/resources/stressTest.csv");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
 		//Start Cassandra and get beans
         ds.sprout("cassandra");
         dms = ds.getContext().getBean( EdmManager.class );
@@ -327,12 +361,12 @@ public class CustomCSVPopulation {
 		CreateSchema();
 		
 		//Write CSV into database
-        //Update path if necessary
-		WriteCSVtoDB( "src/test/resources/" + nameCSV + ".csv" );
+		WriteCSVtoDB( "src/test/resources/stressTest.csv" );
 		
 		//End Cassandra
         ds.plowUnder();
         
 		//At this point, write shell script to make curl requests to server, and time.
+
 	}
 }

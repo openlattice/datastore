5709f075e5f2894a34ebebb12f32b9792358f601
Katherine <katherine@kryptnostic.com>
2017-01-07 18:16:11 -0800

    add search controller

---
+++ src/main/java/com/kryptnostic/datastore/search/controllers/SearchController.java
@@ -0,0 +1,17 @@
+package com.kryptnostic.datastore.search.controllers;
+
+import java.util.Set;
+
+import org.apache.olingo.commons.api.edm.FullQualifiedName;
+
+import com.dataloom.search.SearchApi;
+
+public class SearchController implements SearchApi {
+
+	@Override
+	public Object executeQuery(String query, FullQualifiedName entityType, Set<FullQualifiedName> propertyTypes) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

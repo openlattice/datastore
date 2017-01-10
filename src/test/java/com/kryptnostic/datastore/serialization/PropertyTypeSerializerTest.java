d99c40286f851f902bb79e7b07610332f73ac30d
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-01-09 18:44:55 -0800

    Fix property type serializer test

--- src/test/java/com/kryptnostic/datastore/serialization/PropertyTypeSerializerTest.java
+++ src/test/java/com/kryptnostic/datastore/serialization/PropertyTypeSerializerTest.java
@@ -1,14 +1,12 @@
 package com.kryptnostic.datastore.serialization;
 
-import org.apache.olingo.commons.api.edm.EdmPrimitiveTypeKind;
-import org.apache.olingo.commons.api.edm.FullQualifiedName;
 import org.junit.BeforeClass;
 
 import com.dataloom.data.serializers.FullQualifedNameJacksonDeserializer;
 import com.dataloom.data.serializers.FullQualifedNameJacksonSerializer;
 import com.dataloom.edm.internal.PropertyType;
+import com.dataloom.edm.internal.TestDataFactory;
 import com.dataloom.serializer.BaseJacksonSerializationTest;
-import com.google.common.collect.ImmutableSet;
 
 public class PropertyTypeSerializerTest extends BaseJacksonSerializationTest<PropertyType> {
     @BeforeClass
@@ -21,10 +19,7 @@ public class PropertyTypeSerializerTest extends BaseJacksonSerializationTest<Pro
 
     @Override
     protected PropertyType getSampleData() {
-        return new PropertyType(
-                new FullQualifiedName( "test", "andino" ),
-                ImmutableSet.of(),
-                EdmPrimitiveTypeKind.String );
+        return TestDataFactory.propertyType();
     }
 
     @Override

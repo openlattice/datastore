d302333440873d3caf772a87f0ff263f5bcbf72a
Katherine <katherine@kryptnostic.com>
2017-01-10 17:01:14 -0800

    return json from the json endpoint

--- src/main/java/com/kryptnostic/datastore/search/controllers/SearchController.java
+++ src/main/java/com/kryptnostic/datastore/search/controllers/SearchController.java
@@ -8,6 +8,10 @@ import java.util.UUID;
 import javax.inject.Inject;
 
 import com.dataloom.search.SearchApi;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.google.common.base.Optional;
 import com.kryptnostic.datastore.services.SearchService;
 
@@ -18,7 +22,16 @@ public class SearchController implements SearchApi {
 
 	@Override
 	public String executeQueryJson(String query, UUID entityType, Set<UUID> propertyTypes) {
-		return searchService.executeEntitySetKeywordSearchQuery( query, Optional.of( entityType ), Optional.of( propertyTypes ) ).toString();
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.registerModule( new GuavaModule() );
+		mapper.registerModule( new JodaModule() );
+		try {
+			return mapper.writeValueAsString( searchService
+					.executeEntitySetKeywordSearchQuery( query, Optional.of( entityType ), Optional.of( propertyTypes ) ) );
+		} catch (JsonProcessingException e) {
+			e.printStackTrace();
+		}
+		return "";
 	}
 
 	@Override

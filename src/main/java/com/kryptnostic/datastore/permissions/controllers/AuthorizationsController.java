c26a7df0ed50ed70b5915cd19908df5119be50f5
Ho Chung Siu <hochung@kryptnostic.com>
2017-01-15 18:16:40 -0800

    Add AuthorizationsController

---
+++ src/main/java/com/kryptnostic/datastore/permissions/controllers/AuthorizationsController.java
@@ -0,0 +1,51 @@
+package com.kryptnostic.datastore.permissions.controllers;
+
+import java.util.Map;
+import java.util.Set;
+
+import javax.inject.Inject;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.dataloom.authorization.Auth;
+import com.dataloom.authorization.AuthQuery;
+import com.dataloom.authorization.AuthorizationManager;
+import com.dataloom.authorization.AuthorizationsApi;
+import com.dataloom.authorization.AuthorizingComponent;
+import com.dataloom.authorization.Permission;
+import com.dataloom.authorization.Principals;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Maps;
+
+public class AuthorizationsController implements AuthorizationsApi, AuthorizingComponent {
+    private static final Logger  logger = LoggerFactory.getLogger( AuthorizationsController.class );
+
+    @Inject
+    private AuthorizationManager authorizations;
+
+    @Override
+    @RequestMapping(
+        path = "/" + AUTHORIZATIONS,
+        method = RequestMethod.POST,
+        consumes = MediaType.APPLICATION_JSON_VALUE,
+        produces = MediaType.APPLICATION_JSON_VALUE )
+    public Iterable<Auth> checkAuthorizations( Set<AuthQuery> queries ) {
+        return Iterables.transform( queries, this::getAuth )::iterator;
+    }
+
+    @Override
+    public AuthorizationManager getAuthorizationManager() {
+        return authorizations;
+    }
+
+    private Auth getAuth( AuthQuery query ) {
+        Set<Permission> currentPermissions = authorizations.getSecurableObjectPermissions( query.getAclKey(),
+                Principals.getCurrentPrincipals() );
+        Map<Permission, Boolean> permissionsMap = Maps.asMap( query.getPermissions(), currentPermissions::contains );
+        return new Auth( query.getAclKey(), permissionsMap );
+    }
+}

849297d75f5053179f8aa96ccbfa496d97082138
Katherine <katherine@kryptnostic.com>
2017-01-15 16:27:43 -0800

    format!

--- src/main/java/com/kryptnostic/datastore/search/controllers/SearchController.java
+++ src/main/java/com/kryptnostic/datastore/search/controllers/SearchController.java
@@ -26,55 +26,59 @@ import com.kryptnostic.datastore.services.SearchService;
 
 @RestController
 public class SearchController implements SearchApi {
-	
-	@Inject
-	private SearchService searchService;
-	
-    @RequestMapping(
-    		path = { "/" + SEARCH },
-    		method = RequestMethod.POST,
-    		produces = { MediaType.APPLICATION_JSON_VALUE } )
-	public String executeQueryJson(
-			@RequestParam(
-					value = KEYWORD,
-					required = false ) String query,
-			@RequestParam(
-					value = ENTITY_TYPE_ID,
-					required = false ) UUID entityType,
-			@RequestBody Set<UUID> propertyTypes ) {
-    	if ( query == null && entityType == null && propertyTypes == null ) {
-    		throw new HttpServerErrorException( HttpStatus.BAD_REQUEST, "You must specify at least one query param (keyword 'kw', entity type id 'eid', property type ids 'pid'" );
-    	}
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.registerModule( new GuavaModule() );
-		mapper.registerModule( new JodaModule() );
-		Optional<String> optionalQuery = ( query == null ) ? Optional.absent() : Optional.of( query );
-		Optional<UUID> optionalEntityType = ( entityType == null ) ? Optional.absent() : Optional.of( entityType );
-		Optional<Set<UUID>> optionalPropertyTypes = ( propertyTypes == null ) ? Optional.absent() : Optional.of( propertyTypes );
-		try {
-			return mapper.writeValueAsString( searchService
-					.executeEntitySetKeywordSearchQuery( optionalQuery, optionalEntityType, optionalPropertyTypes ) );
-		} catch (JsonProcessingException e) {
-			e.printStackTrace();
-		}
-		return Lists.newArrayList().toString();
-	}
-    
+
+    @Inject
+    private SearchService searchService;
 
     @RequestMapping(
-    		path = { "/" + SEARCH_JAVA },
-    		method = RequestMethod.POST )
-	public Iterable<Map<String, Object>> executeQuery(
-			@RequestParam(
-					value = KEYWORD,
-					required = false ) String query,
-			@RequestParam(
-					value = ENTITY_TYPE_ID,
-					required = false ) UUID entityType,
-			@RequestBody Set<UUID> propertyTypes ) {
-    	Optional<String> optionalQuery = ( query == null ) ? Optional.absent() : Optional.of( query );
-    	Optional<UUID> optionalEntityType = ( entityType == null ) ? Optional.absent() : Optional.of( entityType );
-		return searchService.executeEntitySetKeywordSearchQuery( optionalQuery, optionalEntityType, Optional.of( propertyTypes ) );
-	}
+        path = { "/" + SEARCH },
+        method = RequestMethod.POST,
+        produces = { MediaType.APPLICATION_JSON_VALUE } )
+    public String executeQueryJson(
+            @RequestParam(
+                value = KEYWORD,
+                required = false ) String query,
+            @RequestParam(
+                value = ENTITY_TYPE_ID,
+                required = false ) UUID entityType,
+            @RequestBody Set<UUID> propertyTypes ) {
+        if ( query == null && entityType == null && propertyTypes == null ) {
+            throw new HttpServerErrorException(
+                    HttpStatus.BAD_REQUEST,
+                    "You must specify at least one query param (keyword 'kw', entity type id 'eid', property type ids 'pid'" );
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule( new GuavaModule() );
+        mapper.registerModule( new JodaModule() );
+        Optional<String> optionalQuery = ( query == null ) ? Optional.absent() : Optional.of( query );
+        Optional<UUID> optionalEntityType = ( entityType == null ) ? Optional.absent() : Optional.of( entityType );
+        Optional<Set<UUID>> optionalPropertyTypes = ( propertyTypes == null ) ? Optional.absent()
+                : Optional.of( propertyTypes );
+        try {
+            return mapper.writeValueAsString( searchService
+                    .executeEntitySetKeywordSearchQuery( optionalQuery, optionalEntityType, optionalPropertyTypes ) );
+        } catch ( JsonProcessingException e ) {
+            e.printStackTrace();
+        }
+        return Lists.newArrayList().toString();
+    }
+
+    @RequestMapping(
+        path = { "/" + SEARCH_JAVA },
+        method = RequestMethod.POST )
+    public Iterable<Map<String, Object>> executeQuery(
+            @RequestParam(
+                value = KEYWORD,
+                required = false ) String query,
+            @RequestParam(
+                value = ENTITY_TYPE_ID,
+                required = false ) UUID entityType,
+            @RequestBody Set<UUID> propertyTypes ) {
+        Optional<String> optionalQuery = ( query == null ) ? Optional.absent() : Optional.of( query );
+        Optional<UUID> optionalEntityType = ( entityType == null ) ? Optional.absent() : Optional.of( entityType );
+        return searchService.executeEntitySetKeywordSearchQuery( optionalQuery,
+                optionalEntityType,
+                Optional.of( propertyTypes ) );
+    }
 
 }

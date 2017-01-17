7d9ed127c6fe2d083f21568ff4f56b4790bd7f02
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-01-17 14:36:33 -0800

    Refactor edm controller tests

--- src/test/java/com/dataloom/datastore/edm/EdmControllerTests.java
+++ src/test/java/com/dataloom/datastore/edm/EdmControllerTests.java
@@ -27,90 +27,62 @@ public class EdmControllerTests extends AuthenticatedRestCallsTest {
     private final static Logger logger = LoggerFactory.getLogger( AuthenticatedRestCallsTest.class );
     private final EdmApi        edm    = getApi( EdmApi.class );
 
-    public Pair<UUID, PropertyType> createPropertyType() {
-        PropertyType p = TestDataFactory.propertyType();
-        UUID propertyTypeId = edm.createPropertyType( p );
-        Assert.assertNotNull( "Property type creation shouldn't return null UUID.", propertyTypeId );
-        return Pair.of( propertyTypeId, p );
+    public PropertyType createPropertyType() {
+        PropertyType expected = TestDataFactory.propertyType();
+        UUID propertyTypeId = edm.createPropertyType( expected );
+
+        Assert.assertNotNull( "Property type creation returned null value.", propertyTypeId );
+
+        PropertyType actual = edm.getPropertyType( propertyTypeId );
+
+        Assert.assertNotNull( "Property type retrieval returned null value.", actual );
+        Assert.assertEquals( "Created and retrieved property type don't match", expected, actual );
+
+        return actual;
     }
 
-    public Pair<UUID, EntityType> createEntityType() {
-        Pair<UUID, PropertyType> k = createPropertyType();
-        Pair<UUID, PropertyType> p1 = createPropertyType();
-        Pair<UUID, PropertyType> p2 = createPropertyType();
+    public EntityType createEntityType() {
+        PropertyType p1 = createPropertyType();
+        PropertyType k = createPropertyType();
+        PropertyType p2 = createPropertyType();
 
-        EntityType e = TestDataFactory.entityType( k.getRight() );
-        e.removePropertyTypes( e.getProperties() );
-        e.addPropertyTypes( ImmutableSet.of( k.getLeft(), p1.getLeft(), p2.getLeft() ) );
-        UUID entityTypeId = edm.createEntityType( e );
+        EntityType expected = TestDataFactory.entityType( k );
+        expected.removePropertyTypes( expected.getProperties() );
+        expected.addPropertyTypes( ImmutableSet.of( k.getId(), p1.getId(), p2.getId() ) );
+        UUID entityTypeId = edm.createEntityType( expected );
         Assert.assertNotNull( "Entity type creation shouldn't return null UUID.", entityTypeId );
-        return Pair.of( entityTypeId, e );
-    }
 
-    public void compareAST( AbstractSecurableType expected, AbstractSecurableType actual ) {
-        Assert.assertEquals( expected.getTitle(), actual.getTitle() );
-        Assert.assertEquals( expected.getDescription(), actual.getDescription() );
-        Assert.assertEquals( expected.getCategory(), actual.getCategory() );
-        Assert.assertEquals( expected.getType(), actual.getType() );
-    }
-
-    public void compareSAST(
-            AbstractSchemaAssociatedSecurableType expected,
-            AbstractSchemaAssociatedSecurableType actual ) {
-        compareAST( expected, actual );
-        Assert.assertEquals( expected.getSchemas(), actual.getSchemas() );
-    }
-
-    public void compare( PropertyType expected, PropertyType actual ) {
-        compareSAST( expected, actual );
-        Assert.assertEquals( expected.getDatatype(), actual.getDatatype() );
-    }
-
-    public void compare( EntityType expected, EntityType actual ) {
-        compareSAST( expected, actual );
-        Assert.assertEquals( expected.getProperties(), actual.getProperties() );
-        Assert.assertEquals( expected.getKey(), actual.getKey() );
+        return expected;
     }
 
     @Test
     public void testCreatePropertyType() {
-        Pair<UUID, PropertyType> propertyTypePair = createPropertyType();
-        PropertyType p = edm.getPropertyType( propertyTypePair.getLeft() );
-        Assert.assertNotNull( p );
-        compare( propertyTypePair.getRight(), p );
+        createPropertyType();
     }
 
     @Test
     public void testLookupPropertyTypeByFqn() {
-        Pair<UUID, PropertyType> propertyTypePair = createPropertyType();
-        PropertyType propertyType = edm.getPropertyType( propertyTypePair.getLeft() );
-        Assert.assertNotNull( propertyType );
+        PropertyType propertyType = createPropertyType();
         UUID maybePropertyTypeId = edm.getPropertyTypeId(
                 propertyType.getType().getNamespace(),
                 propertyType.getType().getName() );
         Assert.assertNotNull( maybePropertyTypeId );
-        Assert.assertEquals( propertyTypePair.getLeft(), maybePropertyTypeId );
+        Assert.assertEquals( propertyType.getId(), maybePropertyTypeId );
     }
 
     @Test
     public void testCreateEntityType() {
-        Pair<UUID, EntityType> entityTypePair = createEntityType();
-        EntityType entityType = edm.getEntityType( entityTypePair.getLeft() );
-        UUID maybeEntityTypeId = edm.getEntityTypeId(
-                entityType.getType().getNamespace(),
-                entityType.getType().getName() );
-        Assert.assertNotNull( maybeEntityTypeId );
-        Assert.assertEquals( entityTypePair.getLeft(), maybeEntityTypeId );
+        createEntityType();
     }
 
     @Test
     public void testLookupEntityTypeByFqn() {
-        Pair<UUID, EntityType> entityTypePair = createEntityType();
-        EntityType entityType = edm.getEntityType( entityTypePair.getLeft() );
+        EntityType entityType = createEntityType();
         UUID maybeEntityTypeId = edm.getEntityTypeId(
                 entityType.getType().getNamespace(),
                 entityType.getType().getName() );
-        Assert.assertEquals( entityTypePair.getLeft(), maybeEntityTypeId );
+        Assert.assertNotNull( maybeEntityTypeId );
+        Assert.assertEquals( entityType.getId(), maybeEntityTypeId );
     }
 
     @Test
@@ -121,8 +93,7 @@ public class EdmControllerTests extends AuthenticatedRestCallsTest {
 
     @Test
     public void createEntitySet() {
-        Pair<UUID, EntityType> entityTypePair = createEntityType();
-        EntityType entityType = edm.getEntityType( entityTypePair.getLeft() );
+        EntityType entityType = createEntityType();
 
         EntitySet es = new EntitySet(
                 entityType.getType(),
@@ -130,9 +101,26 @@ public class EdmControllerTests extends AuthenticatedRestCallsTest {
                 "foo",
                 "foobar",
                 Optional.<String> of( "barred" ) );
-        Map<String, UUID> entitySetId = edm.createEntitySets( ImmutableSet.of( es ) );
-        Set<EntitySet> ess = ImmutableSet.copyOf( edm.getEntitySets() );
-        Assert.assertTrue( ess.contains( es ) );
+
+        Set<EntitySet> ees = ImmutableSet.copyOf( edm.getEntitySets() );
+
+        Assert.assertFalse( ees.contains( es ) );
+
+        Map<String, UUID> entitySetIds = edm.createEntitySets( ImmutableSet.of( es ) );
+        entitySetIds.values().contains( es.getId() );
+
+        Set<EntitySet> aes = ImmutableSet.copyOf( edm.getEntitySets() );
+
+        Assert.assertTrue( aes.contains( es ) );
+    }
+
+    @Test
+    public void testGetPropertyTypes() {
+        Set<PropertyType> epts = ImmutableSet.copyOf( edm.getPropertyTypes() );
+        PropertyType propertyType = createPropertyType();
+        Assert.assertFalse( epts.contains( propertyType ) );
+        Set<PropertyType> apts = ImmutableSet.copyOf( edm.getPropertyTypes() );
+        Assert.assertTrue( apts.contains( propertyType ) );
     }
 
     @AfterClass

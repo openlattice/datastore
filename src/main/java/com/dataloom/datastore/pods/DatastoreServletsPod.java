7a34750f62fd6f91997b867a73836f73ddde5cb5
Matthew Tamayo-Rios <matthew@kryptnostic.com>
2017-01-18 12:17:29 -0800

    Adapt servlet path based on environment
    The main reason behind this change is that the AWS loadbalancer seems to be set up to eat the service prefix of "/datastore".

--- src/main/java/com/dataloom/datastore/pods/DatastoreServletsPod.java
+++ src/main/java/com/dataloom/datastore/pods/DatastoreServletsPod.java
@@ -1,20 +1,41 @@
 package com.dataloom.datastore.pods;
 
+import java.util.Set;
+
+import javax.inject.Inject;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
+import com.kryptnostic.rhizome.configuration.ConfigurationConstants.Profiles;
 import com.kryptnostic.rhizome.configuration.servlets.DispatcherServletConfiguration;
 
 @Configuration
 public class DatastoreServletsPod {
 
+    @Inject
+    private Environment environment;
+
     @Bean
     public DispatcherServletConfiguration edmServlet() {
-        return new DispatcherServletConfiguration(
-                "edm",
-                new String[] { "/datastore/*" },
-                1,
-                Lists.<Class<?>> newArrayList( DatastoreMvcPod.class ) );
+        Set<String> profiles = ImmutableSet.copyOf( environment.getActiveProfiles() );
+
+        if ( profiles.contains( Profiles.AWS_CONFIGURATION_PROFILE ) ) {
+            return new DispatcherServletConfiguration(
+                    "edm",
+                    new String[] { "/*" },
+                    1,
+                    Lists.<Class<?>> newArrayList( DatastoreMvcPod.class ) );
+        } else if ( profiles.contains( Profiles.LOCAL_CONFIGURATION_PROFILE ) ) {
+            return new DispatcherServletConfiguration(
+                    "edm",
+                    new String[] { "/datastore/*" },
+                    1,
+                    Lists.<Class<?>> newArrayList( DatastoreMvcPod.class ) );
+        }
+        return null;
     }
 }

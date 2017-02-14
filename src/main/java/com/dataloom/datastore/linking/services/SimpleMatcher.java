4a4b113a68ee58bc65af46e3dc056b86c64075ae
Ho Chung Siu <hochung@kryptnostic.com>
2017-02-13 23:16:14 -0800

    basic version of matcher

--- src/main/java/com/dataloom/datastore/linking/services/SimpleMatcher.java
+++ src/main/java/com/dataloom/datastore/linking/services/SimpleMatcher.java
@@ -6,12 +6,18 @@ import java.util.Set;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
+
 import com.dataloom.linking.Entity;
 import com.dataloom.linking.components.Matcher;
 import com.dataloom.linking.util.UnorderedPair;
 import com.google.common.collect.Multimap;
 import com.kryptnostic.datastore.services.EdmManager;
 
+/**
+ * The most basic version of Matcher. Use Jaro-Winkler to calculate scores.
+ *
+ */
 public class SimpleMatcher implements Matcher {
 
     private Multimap<UUID, UUID>         linkingMap;
@@ -20,16 +26,38 @@ public class SimpleMatcher implements Matcher {
     private Map<Map<UUID, UUID>, Double> weights;
 
     private final EdmManager             dms;
-
+    
     public SimpleMatcher(
             EdmManager dms ) {
         this.dms = dms;
     }
 
     @Override
-    //TODO implement distance calculator for "standard" features, such as names, that are captured in the getWeight method.
     public double score( UnorderedPair<Entity> entityPair ) {
+        Entity[] entities = entityPair.getAsArray();
+        Entity elem0 = entities[0];
+        Entity elem1 = entities[1];
         
+        double score = 0;
+        for ( Map<UUID, UUID> link : linkingProperties ) {
+            UUID esId0 = elem0.getKey().getEntitySetId();
+            UUID esId1 = elem1.getKey().getEntitySetId();
+            
+            if( link.containsKey( esId0 ) && link.containsKey( esId1 ) ){
+                UUID ptId0 = link.get( esId0 );
+                UUID ptId1 = link.get( esId1 );
+                
+                if( elem0.getProperties().containsKey( ptId0.toString() ) && elem1.getProperties().containsKey( ptId1.toString() ) ){
+                    //TODO assuming singleton right now
+                    //TODO very bad toString
+                    String val0 = elem0.getProperties().get( ptId0.toString() ).toString();
+                    String val1 = elem1.getProperties().get( ptId1.toString() ).toString();
+                    
+                    score += getScore( link, val0, val1 ) * getWeight( link );
+                }
+            }
+        }
+        return score;
     }
 
     @Override
@@ -70,11 +98,15 @@ public class SimpleMatcher implements Matcher {
         }
     }
 
+    private Set<String> getPropertyNames( Map<UUID, UUID> link ){
+        return dms.getPropertyTypes( new HashSet<>( link.values() ) ).stream()
+        .map( pt -> pt.getType().getName() ).collect( Collectors.toSet() );
+    }    
+
     // TODO lolz
     // Right now, 10 is something with heavy weight, 1 is smallest.
     private double getWeight( Map<UUID, UUID> link ) {
-        Set<String> propertyNames = dms.getPropertyTypes( new HashSet<>( link.values() ) ).stream()
-                .map( pt -> pt.getType().getName() ).collect( Collectors.toSet() );
+        Set<String> propertyNames = getPropertyNames( link );
         if ( propertyNames.contains( "year" ) || propertyNames.contains( "date" ) || propertyNames.contains( "dob" )
                 || propertyNames.contains( "id" ) || propertyNames.contains( "ssn" ) ) {
             return 10;
@@ -83,5 +115,16 @@ public class SimpleMatcher implements Matcher {
             return 7;
         }
         return 0;
+    }
+    
+    /**
+     * Use Jaro-Winkler for now.
+     * @param link
+     * @param val0
+     * @param val1
+     * @return
+     */
+    private double getScore( Map<UUID, UUID> link, String val0, String val1 ){
+        return StringUtils.getJaroWinklerDistance( val0, val1 );
     }
 }

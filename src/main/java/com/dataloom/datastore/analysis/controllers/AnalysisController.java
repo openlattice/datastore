a2b784f128655000c8f6f51c44e4d7669677f229
Katherine <katherine@kryptnostic.com>
2017-04-27 17:01:10 -0700

    download top utilizers as csv, EntitySetData takes generic

--- src/main/java/com/dataloom/datastore/analysis/controllers/AnalysisController.java
+++ src/main/java/com/dataloom/datastore/analysis/controllers/AnalysisController.java
@@ -1,18 +1,21 @@
 package com.dataloom.datastore.analysis.controllers;
 
 import java.util.EnumSet;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dataloom.analysis.AnalysisApi;
@@ -20,9 +23,9 @@ import com.dataloom.analysis.requests.TopUtilizerDetails;
 import com.dataloom.authorization.EdmAuthorizationHelper;
 import com.dataloom.authorization.Permission;
 import com.dataloom.data.EntitySetData;
+import com.dataloom.datastore.constants.CustomMediaType;
 import com.dataloom.datastore.services.AnalysisService;
 import com.dataloom.edm.type.PropertyType;
-import com.google.common.collect.Lists;
 import com.google.common.collect.SetMultimap;
 import com.kryptnostic.datastore.services.EdmService;
 
@@ -39,20 +42,52 @@ public class AnalysisController implements AnalysisApi {
     @Inject
     private EdmAuthorizationHelper authorizationsHelper;
 
+    private static void setDownloadContentType( HttpServletResponse response, FileType fileType ) {
+        if ( fileType == FileType.csv ) {
+            response.setContentType( CustomMediaType.TEXT_CSV_VALUE );
+        } else {
+            response.setContentType( MediaType.APPLICATION_JSON_VALUE );
+        }
+    }
+
     @RequestMapping(
         path = { ENTITY_SET_ID_PATH + NUM_RESULTS_PATH },
-        method = RequestMethod.POST )
-    @Override
-    public Iterable<SetMultimap<Object, Object>> getTopUtilizers(
+        method = RequestMethod.POST,
+        produces = { MediaType.APPLICATION_JSON_VALUE, CustomMediaType.TEXT_CSV_VALUE } )
+    public EntitySetData<Object> getTopUtilizers(
             @PathVariable( ENTITY_SET_ID ) UUID entitySetId,
             @PathVariable( NUM_RESULTS ) int numResults,
-            @RequestBody List<TopUtilizerDetails> topUtilizerDetails ) {
-        if ( topUtilizerDetails.size() == 0 ) return Lists.newArrayList();
+            @RequestBody List<TopUtilizerDetails> topUtilizerDetails,
+            @RequestParam(
+                value = FILE_TYPE,
+                required = false ) FileType fileType,
+            HttpServletResponse response ) {
+        FileType downloadType = ( fileType == null ) ? FileType.json : fileType;
+        // setContentDisposition( response, entitySetId.toString(), downloadType );
+        setDownloadContentType( response, downloadType );
+        return getTopUtilizers( entitySetId, numResults, topUtilizerDetails, downloadType );
+    }
+
+    @Override
+    public EntitySetData<Object> getTopUtilizers(
+            UUID entitySetId,
+            int numResults,
+            List<TopUtilizerDetails> topUtilizerDetails,
+            FileType fileType ) {
+        if ( topUtilizerDetails.size() == 0 ) return null;
         Set<UUID> authorizedProperties = authorizationsHelper.getAuthorizedPropertiesOnEntitySet( entitySetId,
                 EnumSet.of( Permission.READ ) );
         if ( authorizedProperties.size() == 0 ) return null;
         Map<UUID, PropertyType> authorizedPropertyTypes = edm.getPropertyTypesAsMap( authorizedProperties );
-        return analysisService.getTopUtilizers( entitySetId, numResults, topUtilizerDetails, authorizedPropertyTypes );
+        Iterable<SetMultimap<Object, Object>> utilizers = analysisService.getTopUtilizers( entitySetId,
+                numResults,
+                topUtilizerDetails,
+                authorizedPropertyTypes );
+        Set<Object> columnTitles = authorizedPropertyTypes.values().stream().map( pt -> pt.getType() )
+                .collect( Collectors.toSet() );
+        columnTitles.add( "count" );
+        columnTitles.add( "id" );
+        return new EntitySetData<Object>( columnTitles, utilizers );
     }
 
 }

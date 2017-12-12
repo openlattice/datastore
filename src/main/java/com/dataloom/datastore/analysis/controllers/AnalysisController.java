e84516d78b47afb369782c5a69a670137b2735b5
Katherine Bernstein <katherine@openlattice.com>
2017-12-11 16:49:38 -0800

    load entity set associations + neighbors

--- src/main/java/com/dataloom/datastore/analysis/controllers/AnalysisController.java
+++ src/main/java/com/dataloom/datastore/analysis/controllers/AnalysisController.java
@@ -11,6 +11,9 @@ import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletResponse;
 
+import com.dataloom.analysis.requests.NeighborType;
+import com.dataloom.authorization.*;
+import com.openlattice.authorization.AclKey;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,8 +24,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.dataloom.analysis.AnalysisApi;
 import com.dataloom.analysis.requests.TopUtilizerDetails;
-import com.dataloom.authorization.EdmAuthorizationHelper;
-import com.dataloom.authorization.Permission;
 import com.dataloom.data.EntitySetData;
 import com.dataloom.data.requests.FileType;
 import com.dataloom.datastore.constants.CustomMediaType;
@@ -33,16 +34,19 @@ import com.kryptnostic.datastore.services.EdmService;
 
 @RestController
 @RequestMapping( AnalysisApi.CONTROLLER )
-public class AnalysisController implements AnalysisApi {
+public class AnalysisController implements AnalysisApi, AuthorizingComponent {
 
     @Inject
-    private AnalysisService        analysisService;
+    private AnalysisService analysisService;
 
     @Inject
-    private EdmService             edm;
+    private EdmService edm;
 
     @Inject
     private EdmAuthorizationHelper authorizationsHelper;
+
+    @Inject
+    private AuthorizationManager authorizations;
 
     private static void setDownloadContentType( HttpServletResponse response, FileType fileType ) {
         if ( fileType == FileType.csv ) {
@@ -53,17 +57,18 @@ public class AnalysisController implements AnalysisApi {
     }
 
     @RequestMapping(
-        path = { ENTITY_SET_ID_PATH + NUM_RESULTS_PATH },
-        method = RequestMethod.POST,
-        produces = { MediaType.APPLICATION_JSON_VALUE, CustomMediaType.TEXT_CSV_VALUE } )
+            path = { ENTITY_SET_ID_PATH + NUM_RESULTS_PATH },
+            method = RequestMethod.POST,
+            produces = { MediaType.APPLICATION_JSON_VALUE, CustomMediaType.TEXT_CSV_VALUE } )
     public EntitySetData<Object> getTopUtilizers(
             @PathVariable( ENTITY_SET_ID ) UUID entitySetId,
             @PathVariable( NUM_RESULTS ) int numResults,
             @RequestBody List<TopUtilizerDetails> topUtilizerDetails,
             @RequestParam(
-                value = FILE_TYPE,
-                required = false ) FileType fileType,
+                    value = FILE_TYPE,
+                    required = false ) FileType fileType,
             HttpServletResponse response ) {
+        ensureReadAccess( new AclKey( entitySetId ) );
         FileType downloadType = ( fileType == null ) ? FileType.json : fileType;
         // setContentDisposition( response, entitySetId.toString(), downloadType );
         setDownloadContentType( response, downloadType );
@@ -76,10 +81,12 @@ public class AnalysisController implements AnalysisApi {
             int numResults,
             List<TopUtilizerDetails> topUtilizerDetails,
             FileType fileType ) {
-        if ( topUtilizerDetails.size() == 0 ) return null;
+        if ( topUtilizerDetails.size() == 0 )
+            return null;
         Set<UUID> authorizedProperties = authorizationsHelper.getAuthorizedPropertiesOnEntitySet( entitySetId,
                 EnumSet.of( Permission.READ ) );
-        if ( authorizedProperties.size() == 0 ) return null;
+        if ( authorizedProperties.size() == 0 )
+            return null;
         Map<UUID, PropertyType> authorizedPropertyTypes = edm.getPropertyTypesAsMap( authorizedProperties );
         Iterable<SetMultimap<Object, Object>> utilizers = analysisService.getTopUtilizers( entitySetId,
                 numResults,
@@ -93,4 +100,17 @@ public class AnalysisController implements AnalysisApi {
         return new EntitySetData<Object>( columnTitles, utilizers );
     }
 
+    @RequestMapping(
+            path = { ENTITY_SET_ID_PATH + TYPES_PATH },
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE )
+    @Override
+    public Iterable<NeighborType> getNeighborTypes( @PathVariable( ENTITY_SET_ID ) UUID entitySetId ) {
+        ensureReadAccess( new AclKey( entitySetId ) );
+        return analysisService.getNeighborTypes( entitySetId );
+    }
+
+    @Override public AuthorizationManager getAuthorizationManager() {
+        return authorizations;
+    }
 }
